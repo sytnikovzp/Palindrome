da2843a58599c5774963ccee1f84b4a825dbe9ef
alex <sytnikov.zp@gmail.com>
2024-02-12 00:02:46 +0200

    Add palindrome function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const str = 'Аргентина МАНИТ негра';
+const sep = '';
+
+const reverseLine = (str) => {
+  const sep = '';
+  const strWithoutGaps = str.toLowerCase().replaceAll(' ', sep);
+  const reverseString = strWithoutGaps.split(sep).reverse().join(sep);
+  return strWithoutGaps === reverseString;
+};
+
+console.log(reverseLine(str));
